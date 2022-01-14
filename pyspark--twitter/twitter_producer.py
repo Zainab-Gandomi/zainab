78937d08aea0741f16337f204f87fe34e549b9f3
Zainab-Gandomi <79536268+Zainab-Gandomi@users.noreply.github.com>
2022-01-14 21:30:05 +0000

    Create twitter_producer.py

---
+++ pyspark--twitter/twitter_producer.py
@@ -0,0 +1,66 @@
+import tweepy
+#from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from kafka import KafkaProducer, KafkaConsumer
+import time
+
+
+access_token = "  "
+access_token_secret = "   "
+consumer_key = "   "
+consumer_secret = "    "
+
+#creating the authentication object
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+#setting access token
+auth.set_access_token(access_token, access_token_secret)
+
+#creating the API object by passing in auth information
+api = tweepy.API(auth)
+
+ 
+
+from datetime import datetime
+
+def normalize_timestmp(time):
+mytime = datatime.strptime(time, "%Y-%m-%d %H:%M:%S")
+mytime.strftime( "%Y-%m-%d %H:%M:%S")
+
+ 
+
+producer = KafkaProducer(bootstrap_servers ="localhost:9092")
+topic_name = 'Twitter'
+
+def get_twitter_data():
+key_words = api.search_tweets(q = "covid", lang ="en")
+for i in key_words:
+record = ''
+record += str(i.user.id_str)
+record += ';'
+record += str(normalize_timestamp(str(i.create_at)))
+record += ';'
+record += str(i.user.followrs_count)
+record += ';'
+record += str(i.user.location)
+record += ';'
+record += str(i.favorate_count)
+record += ';'
+record += str(i.retweet_count)
+producer.send(topic_name, str.encode(record))
+
+ 
+
+get_twitter_data()
+
+def periodic_work(interval):
+while True:
+get_twitter_data()
+#interval should be an integer, the number of second to wait
+time.sleep(interval)
+
+# get data every couple of minute
+periodic_work(60 * 0.1)
+
+ 
